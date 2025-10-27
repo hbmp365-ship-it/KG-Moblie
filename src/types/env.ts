e7b8c1d259ab196e0be6f2946e27e7b135b79bff
서준호 <slsldldlcx@gmail.com>
2025-10-27 14:15:00 +0900

    기본설정 유지

--- src/types/env.ts
+++ src/types/env.ts
@@ -2,4 +2,10 @@ export interface Env {
   KG_MERCHANT_ID: string;
   KG_MERCHANT_KEY: string;
   KG_API_URL: string;
+  KG_SERVICE_ID: string;
+  KG_CREDIT_CARD_SERVICE_ID: string;
+  KG_MOBILE_SERVICE_ID: string;
+  KG_EASY_PAY_SERVICE_ID: string;
+  KG_ACCOUNT_TRANSFER_SERVICE_ID: string;
+  KG_VIRTUAL_ACCOUNT_SERVICE_ID: string;
 }
